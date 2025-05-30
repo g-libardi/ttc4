430f07d1535498e34d267a7e78ac2d0685c92b96
g-libardi <contato@libardi.dev.br>
2025-05-30 07:45:45 -0300

    Refactor BGP topology implementation and update project configuration
    - Removed legacy entry point scripts for the BGP topology.
    - Updated `pyproject.toml` to include a new script entry point for running the topology.
    - Changed package name in `uv.lock` from "hostshare" to "atividades".
    - Adjusted the mount path in the VM run script to point to the 'atividades' directory.

--- vm/run-vn.sh
+++ vm/run-vn.sh
@@ -2,4 +2,4 @@
 
 
 QEMU_KERNEL_PARAMS=console=ttyS0 ./result/bin/run-nixos-vm -nographic \
-  -virtfs local,path=$(pwd)/../pratica_01,mount_tag=hostshare,security_model=none
+  -virtfs local,path=$(pwd)/../atividades,mount_tag=hostshare,security_model=none
